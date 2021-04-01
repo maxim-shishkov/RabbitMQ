d1d70c029694c4d1606a5ade11cac270cbed4023
Maxim Shishkov <shishkov.maxim@yandex.ru>
2021-04-01 14:22:03 +0300

    Fix worker

--- main.go
+++ main.go
@@ -11,64 +11,64 @@ const (
 	TIMEFORMAT = "15:04:05"
 )
 
-	func main() {
-		rst := make(chan struct{})
+func main() {
+	rst := make(chan struct{})
 
-		go workerRabbitMQ(rst)
+	go workerRabbitMQ(rst)
 
-		for{
-			select {
-			case <-rst:
-				log.Println("Restarting Rabbit MQ")
-				time.Sleep(1 * time.Second)
-				go workerRabbitMQ(rst)
-			}
+	for{
+		select {
+		case <-rst:
+			log.Println("Restarting Rabbit MQ")
+			time.Sleep(1 * time.Second)
+			go workerRabbitMQ(rst)
+		}
+	}
+}
+
+func workerRabbitMQ(rst chan struct{}) {
+	conn, err := amqp.Dial(URL)
+	if err != nil {
+		log.Printf("Failed to connect to RabbitMQ: %s", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Printf("Failed to open a channel: %s", err)
+	}
+	defer ch.Close()
+
+	errorAmqp := ch.NotifyClose(make(chan *amqp.Error, 1))
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+
+	for {
+		select {
+			case e := <- errorAmqp:
+				log.Printf("chan error: %s", e.Error())
+				rst <- struct{}{}
+			case m := <-msgs:
+				log.Printf("Received a message: %s", m.Body)
 		}
 	}
 
-	func workerRabbitMQ(rst chan struct{}) {
-		conn, err := amqp.Dial(URL)
-		if err != nil {
-			log.Printf("Failed to connect to RabbitMQ: %s", err)
-		}
-		defer conn.Close()
-
-		ch, err := conn.Channel()
-		if err != nil {
-			log.Printf("Failed to open a channel: %s", err)
-		}
-		defer ch.Close()
-
-		errorAmqp := ch.NotifyClose(make(chan *amqp.Error, 1))
-
-		q, err := ch.QueueDeclare(
-			"hello", // name
-			false,   // durable
-			false,   // delete when unused
-			false,   // exclusive
-			false,   // no-wait
-			nil,     // arguments
-		)
-		msgs, err := ch.Consume(
-			q.Name, // queue
-			"",     // consumer
-			true,   // auto-ack
-			false,  // exclusive
-			false,  // no-local
-			false,  // no-wait
-			nil,    // args
-		)
-
-		for {
-			select {
-				case e := <- errorAmqp:
-					log.Printf("chan error: %s", e.Error())
-					rst <- struct{}{}
-				case m := <-msgs:
-					log.Printf("Received a message: %s", m.Body)
-			}
-		}
-
-	}
+}
 
 
